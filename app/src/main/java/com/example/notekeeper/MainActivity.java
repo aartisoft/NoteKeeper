b7bf87bd09a9381fe8e9901e674d89bc9b655b34
vinnykawa <vinnykawa@gmail>
2020-05-08 15:21:51 +0300

    listView

--- app/src/main/java/com/example/notekeeper/MainActivity.java
+++ app/src/main/java/com/example/notekeeper/MainActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView noteList;
 
     public Button next;
 
@@ -27,5 +33,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
+        noteList=(ListView)findViewById(R.id.list);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Note 1");
+        arrayList.add("Note 2");
+        arrayList.add("Note 3");
+        arrayList.add("Note 4");
+        arrayList.add("Note 5");
+        arrayList.add("Note 6");
+        arrayList.add("Note 7");
+        arrayList.add("Note 8");
+        arrayList.add("Note 9");
+        arrayList.add("Note 10");
+        arrayList.add("Note 11");
+        arrayList.add("Note 12");
+        arrayList.add("Note 13"); 
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MainActivity.this,android.R.layout.simple_list_item_1,arrayList);
+        noteList.setAdapter(arrayAdapter);
     }
 }
